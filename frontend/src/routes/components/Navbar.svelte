416323d3766930cb008611ddd784d64144a4b591
MGM103 <marcus.marinelli3@gmail.com>
2024-08-14 15:08:58 +1000

    Creating frontend skeleton for GoFundMe

---
+++ frontend/src/routes/components/Navbar.svelte
@@ -0,0 +1,17 @@
+<nav>
+    <ul>
+        <li><a>I am a link</a></li>
+    </ul>
+    <button>Connect</button>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+        max-width: 950px;
+        align-items: center;
+        margin: 0 auto;
+    }
+</style>
